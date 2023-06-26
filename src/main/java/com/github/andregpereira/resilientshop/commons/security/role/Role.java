f11d270228dc0c0f324bb3e11f6d3472c1c0b7be
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-06-26 10:46:48 -0300

    Criar enum Role

---
+++ src/main/java/com/github/andregpereira/resilientshop/commons/security/role/Role.java
@@ -0,0 +1,30 @@
+package com.github.andregpereira.resilientshop.commons.security.role;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpMethod;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Stream;
+
+@RequiredArgsConstructor
+public enum Role {
+    ADMIN(Map.of(HttpMethod.GET, Set.of("/**"), HttpMethod.POST, Set.of("/**"), HttpMethod.PUT, Set.of("/**"),
+            HttpMethod.DELETE, Set.of("/**"), HttpMethod.PATCH, Set.of("/**"))),
+    USER(Map.of(HttpMethod.GET, Set.of("/usuarios/{id}", "/produtos/**", "/enderecos/usuario/{idUsuario}",
+                    "/enderecos/usuario/{idUsuario}/apelido", "/pedidos/**"), HttpMethod.POST,
+            Set.of("/enderecos/**", "/pedidos/**"), HttpMethod.PUT, Set.of("/usuarios/**", "/enderecos/**"),
+            HttpMethod.PATCH, Set.of("/usuarios/**"), HttpMethod.DELETE,
+            Set.of("/usuarios/{id}", "/enderecos/{id}", "/pedidos/**"))),
+    ANONYMOUS(Map.of(HttpMethod.GET, Set.of("/produtos/**"), HttpMethod.POST, Set.of("/usuarios/**", "/auth/**")));
+    private final Map<HttpMethod, Set<String>> permissions;
+
+    public static Role getRole(String nome) {
+        return Stream.of(values()).filter(r -> r.name().equalsIgnoreCase(nome)).findFirst().orElseGet(() -> ANONYMOUS);
+    }
+
+    public Set<String> getPermissions(HttpMethod method) {
+        return permissions.getOrDefault(method, Collections.emptySet());
+    }
+}
