819ac8fb6ca9345d5ee331786bc01f3ff559989c
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-07-05 16:48:56 -0300

    Adtualizar Role

--- src/main/java/com/github/andregpereira/resilientshop/commons/security/role/Role.java
+++ src/main/java/com/github/andregpereira/resilientshop/commons/security/role/Role.java
@@ -2,6 +2,7 @@ package com.github.andregpereira.resilientshop.commons.security.role;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.core.GrantedAuthority;
 
 import java.util.Collections;
 import java.util.Map;
@@ -9,13 +10,13 @@ import java.util.Set;
 import java.util.stream.Stream;
 
 @RequiredArgsConstructor
-public enum Role {
+public enum Role implements GrantedAuthority {
     ADMIN(Map.of(HttpMethod.GET, Set.of("/**"), HttpMethod.POST, Set.of("/**"), HttpMethod.PUT, Set.of("/**"),
             HttpMethod.DELETE, Set.of("/**"), HttpMethod.PATCH, Set.of("/**"))),
     USER(Map.of(HttpMethod.GET, Set.of("/usuarios/{id}", "/produtos/**", "/enderecos/usuario/{idUsuario}",
                     "/enderecos/usuario/{idUsuario}/apelido", "/pedidos/**"), HttpMethod.POST,
             Set.of("/enderecos/**", "/pedidos/**"), HttpMethod.PUT, Set.of("/usuarios/**", "/enderecos/**"),
-            HttpMethod.PATCH, Set.of("/usuarios/**"), HttpMethod.DELETE,
+            HttpMethod.PATCH, Set.of("/usuarios/**", "/auth/**"), HttpMethod.DELETE,
             Set.of("/usuarios/{id}", "/enderecos/{id}", "/pedidos/**"))),
     ANONYMOUS(Map.of(HttpMethod.GET, Set.of("/produtos/**"), HttpMethod.POST, Set.of("/usuarios/**", "/auth/**")));
     private final Map<HttpMethod, Set<String>> permissions;
@@ -26,5 +27,10 @@ public enum Role {
 
     public Set<String> getPermissions(HttpMethod method) {
         return permissions.getOrDefault(method, Collections.emptySet());
+    }
+
+    @Override
+    public String getAuthority() {
+        return null;
     }
 }
