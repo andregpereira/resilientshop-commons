bb1194766bf5cb0fa32e0c79875aa0c565b4a428
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-09-05 11:48:29 -0300

    Refatorar código

--- src/main/java/com/github/andregpereira/resilientshop/commons/entities/Usuario.java
+++ src/main/java/com/github/andregpereira/resilientshop/commons/entities/Usuario.java
@@ -3,17 +3,13 @@ package com.github.andregpereira.resilientshop.commons.entities;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonFormat.Shape;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Objects;
-import java.util.StringJoiner;
 
-@Getter
-@Setter
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Usuario {
@@ -32,31 +28,5 @@ public class Usuario {
 
     private boolean ativo;
     private List<Endereco> enderecos;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o)
-            return true;
-        if (!(o instanceof Usuario usuario))
-            return false;
-        return ativo == usuario.ativo && Objects.equals(id, usuario.id) && Objects.equals(nome,
-                usuario.nome) && Objects.equals(sobrenome, usuario.sobrenome) && Objects.equals(cpf,
-                usuario.cpf) && Objects.equals(telefone, usuario.telefone) && Objects.equals(dataCriacao,
-                usuario.dataCriacao) && Objects.equals(dataModificacao, usuario.dataModificacao) && Objects.equals(
-                enderecos, usuario.enderecos);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, nome, sobrenome, cpf, telefone, dataCriacao, dataModificacao, ativo, enderecos);
-    }
-
-    @Override
-    public String toString() {
-        return new StringJoiner(", ", Usuario.class.getSimpleName() + "[", "]").add("id=" + id).add(
-                "nome='" + nome + "'").add("sobrenome='" + sobrenome + "'").add("cpf='" + cpf + "'").add(
-                "telefone='" + telefone + "'").add("dataCriacao=" + dataCriacao).add(
-                "dataModificacao=" + dataModificacao).add("ativo=" + ativo).add("enderecos=" + enderecos).toString();
-    }
 
 }
